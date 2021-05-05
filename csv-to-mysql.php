ec2eda2d4c8d2751ceae6fa692f7c9806589a20e
Jeffrey Wang <jeffw16@users.noreply.github.com>
2021-05-05 15:21:24 -0500

    Update csv-to-mysql.php

--- csv-to-mysql.php
+++ csv-to-mysql.php
@@ -7,6 +7,25 @@
  * Note: If LOAD DATA LOCAL INFILE disabled, enable it in my.cnf using local_infile = 1
  */
 
+/*
+    MediaWiki SQLite to MySQL Conversion
+    Copyright (C) 2021 MyWikis LLC
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 $host = '';
 $username = '';
 $password = '';
